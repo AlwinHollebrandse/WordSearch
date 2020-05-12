7e62d76e454553725491a227fe931a691894b412
Alwin Hollebrandse <hollebrandsaj@mymail.vcu.edu>
2020-05-12 11:13:31 -0400

    basic python server setup

---
+++ server.py
@@ -0,0 +1,25 @@
+# based on code explained here: https://auth0.com/blog/developing-restful-apis-with-python-and-flask/
+
+from flask import Flask, jsonify, request
+
+app = Flask(__name__)
+
+incomes = [
+  { 'description': 'salary', 'amount': 5000 }
+]
+
+
+@app.route('/incomes')
+def get_incomes():
+  return jsonify(incomes)
+
+
+@app.route('/incomes', methods=['POST'])
+def add_income():
+  incomes.append(request.get_json())
+  return '', 204
+
+
+@app.route("/")
+def hello_world():
+  return "Hello, World!"
