50aad70eae88b0fe546b9f11eafd16b310c23380
Alwin Hollebrandse <hollebrandsaj@mymail.vcu.edu>
2020-06-14 13:33:51 -0400

    moved wordSearch code to api folder and made a slideshow esc ui portion

---
+++ ui/wordSearch/src/app/app.module.ts
@@ -0,0 +1,21 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
+
+
+import { AppComponent } from './app.component';
+import { SliderComponent } from './slider/slider.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    SliderComponent
+  ],
+  imports: [
+    BrowserModule,
+    HttpClientModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
